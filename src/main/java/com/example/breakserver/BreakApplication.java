e0df229a27376ead160e18c4f7abfbeeee617de4
mashihara <mashiharahiroyuki@gmail.com>
2017-07-29 16:32:42 +0900

    画面資材も更新

--- src/main/java/com/example/breakserver/BreakApplication.java
+++ src/main/java/com/example/breakserver/BreakApplication.java
@@ -2,8 +2,11 @@ package com.example.breakserver;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.boot.autoconfigure.domain.EntityScan;
+import org.springframework.data.jpa.convert.threeten.Jsr310JpaConverters;
 
 @SpringBootApplication
+@EntityScan(basePackageClasses = {BreakApplication.class, Jsr310JpaConverters.class})
 public class BreakApplication {
 
 	public static void main(String[] args) {
--- src/main/java/com/example/breakserver/domain/Voice.java
+++ src/main/java/com/example/breakserver/domain/Voice.java
@@ -1,11 +1,18 @@
 package com.example.breakserver.domain;
 
 import java.io.Serializable;
+import java.time.LocalTime;
+import java.util.Date;
 
 import javax.persistence.Entity;
+import javax.persistence.EntityListeners;
 import javax.persistence.GeneratedValue;
 import javax.persistence.GenerationType;
 import javax.persistence.Id;
+
+import org.springframework.data.annotation.CreatedBy;
+import org.springframework.data.annotation.CreatedDate;
+import org.springframework.data.jpa.domain.support.AuditingEntityListener;
 
 import lombok.AllArgsConstructor;
 import lombok.Data;
@@ -15,10 +22,8 @@ import lombok.NoArgsConstructor;
 @Data
 @NoArgsConstructor
 @AllArgsConstructor
+@EntityListeners(AuditingEntityListener.class)
 public class Voice implements Serializable {
-	/**
-	 * 
-	 */
 	private static final long serialVersionUID = 1L;
 	@Id
 	@GeneratedValue(strategy=GenerationType.IDENTITY) //postgersの場合、GeneratedValueにこれがいる
@@ -26,7 +31,8 @@ public class Voice implements Serializable {
 	private String voiceContent;
 	private String tentativeUserId;
 	private String deviceSerialId;
-	
+	@CreatedDate private LocalTime createdHinichi;
+
 	public Voice(String voiceContent,String tentativeUserId,String deviceSerialId){
 		this.voiceContent=voiceContent;
 		this.tentativeUserId=tentativeUserId;
