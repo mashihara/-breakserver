f7a8f6231afe120bb9bab696dbd51861fac3a015
mashihara <mashiharahiroyuki@gmail.com>
2017-07-29 16:31:04 +0900

    画面修正

--- src/main/java/com/example/breakserver/domain/Voice.java
+++ src/main/java/com/example/breakserver/domain/Voice.java
@@ -2,7 +2,10 @@ package com.example.breakserver.domain;
 
 import java.io.Serializable;
 
-import javax.persistence.*;
+import javax.persistence.Entity;
+import javax.persistence.GeneratedValue;
+import javax.persistence.GenerationType;
+import javax.persistence.Id;
 
 import lombok.AllArgsConstructor;
 import lombok.Data;
